0375e3127fd6a9352087b9430e4d33bd47161c88
DESKTOP-LG54DD7\Camille <camille.payen.pro@gmail.com>
2023-11-21 18:59:23 +0100

    feat(api): Minor refactoring

--- src/WebApi/Consumers/AlertCreatedConsumer.cs
+++ src/WebApi/Consumers/AlertCreatedConsumer.cs
@@ -6,23 +6,13 @@ using WebApi.SignalR;
 
 namespace WebApi.Consumers;
 
-public class AlertCreatedConsumer : IConsumer<AlertCreated>
+public class AlertCreatedConsumer(ILogger<AlertCreatedConsumer> logger, MessageHub hub, IRepository<Alert> repository)
+    : IConsumer<AlertCreated>
 {
-    private readonly IRepository<Alert> _repository;
-    private readonly MessageHub _hub;
-    private readonly ILogger<AlertCreatedConsumer> _logger;
-
-    public AlertCreatedConsumer(ILogger<AlertCreatedConsumer> logger, MessageHub hub, IRepository<Alert> repository)
-    {
-        _repository = repository;
-        _hub = hub;
-        _logger = logger;
-    }
-    
     public async Task Consume(ConsumeContext<AlertCreated> context)
     {
         var message = context.Message;
-        _logger.LogInformation(
+        logger.LogInformation(
             "Consuming alert: {RaisedAt:T}, level {Level}", 
             message.RaisedAt, message.Level);
         
@@ -33,7 +23,7 @@ public class AlertCreatedConsumer : IConsumer<AlertCreated>
             Level = (AlertLevel)message.Level
         };
 
-        await _hub.SendAlertAsync(alert);
-        await _repository.CreateAsync(alert);
+        await hub.SendAlertAsync(alert);
+        await repository.CreateAsync(alert);
     }
 }
